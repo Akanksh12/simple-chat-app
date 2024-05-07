6a55d4a6d3ca743d3f14c799aabf3dc973340947
Akanksh Chitimalla <55909985+Akanksh12@users.noreply.github.com>
2024-05-07 11:24:58 +0530

    serving any file requested from server at 0.0.0.0 now

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const { createServer } = require("node:http");
-
-const hostname = "127.0.0.1";
+const hostname = "0.0.0.0";
 const port = 6969;
 
+const fs = require('node:fs');
+var mime = require('mime-types');
+
 const server = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Hello World</h1>");
+    // serve index.html
+    if (req.method == 'GET' &&  req.url == '/') { 
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        fs.readFile(__dirname + '/html/index.html', 'utf-8', (err, data) => {
+            if (err) {
+                res.statusCode = 501;
+                console.log(err);
+                res.end(err);
+                return;
+            }
+            res.end(data);
+        })
+    }
+    // serve any other files available
+    
+    if (req.method = 'GET') {
+        fs.readFile(__dirname + req.url, 'utf-8', (err, data) => {
+            res.statusCode = 200;
+            
+            if(err){
+                res.statusCode = 404;
+                res.end('File not found')
+            }
+            res.setHeader('Content-Type', mime.lookup(req.url));
+            res.end(data);
+        })
+    }
 });
 
 server.listen(port, hostname, () => {
