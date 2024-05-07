c8a27c6dc5bc3659cd87a2cb148f64d9ec30c7d3
Akanksh Chitimalla <55909985+Akanksh12@users.noreply.github.com>
2024-05-07 13:57:49 +0530

    added missing mime-type lookup

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ const server = createServer((req, res) => {
                 res.statusCode = 404;
                 res.end(`${req.url} not found`)
             }
+            res.setHeader('Content-Type', mime.lookup(req.url))
             res.end(data);
             return;
         })
